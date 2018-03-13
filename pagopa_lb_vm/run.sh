d4c370ef33b6d5a0270e71bea1d8d1a092660b4c
Federico Feroldi <pix@yahoo.it>
2018-03-13 18:01:42 +0100

    revert back to previous model

--- pagopa_lb_vm/run.sh
+++ pagopa_lb_vm/run.sh
@@ -5,19 +5,9 @@ set -o errexit
 set -o pipefail
 set -o xtrace
 
-VERSION=0.0.2
-
 # install prerequisites
 apt-get update
-apt-get install unzip
 apt-get install ansible
-
-# download the playbook archive
-wget https://github.com/teamdigitale/digital-citizenship-ansible-playbooks/archive/v$(VERSION).zip
-
-unzip v$(VERSION)
-
-cd digital-citizenship-ansible-playbooks-$(VERSION)
 
 # install roles
 ansible-galaxy install -r requirements.yml
