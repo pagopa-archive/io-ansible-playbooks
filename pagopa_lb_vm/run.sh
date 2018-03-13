75c4a1f72a8e739ffb4e082144be01e326400d24
Federico Feroldi <pix@yahoo.it>
2018-03-13 18:13:04 +0100

    going back to single command script

--- pagopa_lb_vm/run.sh
+++ pagopa_lb_vm/run.sh
@@ -5,9 +5,18 @@ set -o errexit
 set -o pipefail
 set -o xtrace
 
+REPO_NAME=digital-citizenship-ansible-playbooks
+VERSION=0.0.4
+
 # install prerequisites
 apt-get update
+apt-get install unzip
 apt-get install ansible
+
+wget https://github.com/teamdigitale/$(REPO_NAME)/archive/v$(VERSION).zip
+
+unzip v$(VERSION).zip
+cd $(REPO_NAME)-$(VERSION)
 
 # install roles
 ansible-galaxy install -r requirements.yml
