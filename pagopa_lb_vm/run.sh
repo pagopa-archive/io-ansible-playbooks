22d9b33f959e7ac35030481779c36f593aca1f3e
Federico Feroldi <pix@yahoo.it>
2018-03-13 16:06:15 +0100

    Playbook for provisioning the pagopa loadbalancer

---
+++ pagopa_lb_vm/run.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# see https://sap1ens.com/blog/2017/07/01/bash-scripting-best-practices/
+set -o errexit
+set -o pipefail
+set -o xtrace
+
+# install ansible
+apt-get update
+apt-get install ansible
+
+# install roles
+ansible-galaxy install -r requirements.yml
+
+# run playbook
+ansible-playbook -K main.yml
+
+# signal successful execution
+exit 0
