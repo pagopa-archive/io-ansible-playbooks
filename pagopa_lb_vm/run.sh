8767a059e0029002bb080d3a2ec15028da0c6517
Federico Feroldi <pix@yahoo.it>
2018-03-13 17:58:08 +0100

    Makes the run script the only entrypoint

--- pagopa_lb_vm/run.sh
+++ pagopa_lb_vm/run.sh
@@ -5,9 +5,19 @@ set -o errexit
 set -o pipefail
 set -o xtrace
 
-# install ansible
+VERSION=0.0.2
+
+# install prerequisites
 apt-get update
+apt-get install unzip
 apt-get install ansible
+
+# download the playbook archive
+wget https://github.com/teamdigitale/digital-citizenship-ansible-playbooks/archive/v$(VERSION).zip
+
+unzip v$(VERSION)
+
+cd digital-citizenship-ansible-playbooks-$(VERSION)
 
 # install roles
 ansible-galaxy install -r requirements.yml
